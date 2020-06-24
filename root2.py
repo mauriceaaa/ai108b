a5e207cc2f7d29b256be2747759ebf4f656151a9
mauriceaaa <9400367@gmail.com>
2020-06-24 23:56:59 +0800

    2020

---
+++ root2.py
@@ -0,0 +1,10 @@
+def root(a,b,c):
+    t = b*b - 4*a*c
+    if (t >= 0):
+        t2 = math.sqrt(t) #無實根
+    else :
+        t2 = cmath.sqrt(t) #有虛根
+    return [(-b+t2)/(2*a), (-b-t2)/(2*a)]
+
+
+print("root of 3x^2+7x+7=", root(3,7,7))
